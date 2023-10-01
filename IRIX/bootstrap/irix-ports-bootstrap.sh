425e79e3da8be905583ee0f8ea9f7518c53fd52c
Zebity Spring <zebity@yahoo.com>
2023-10-02 00:45:47 +1100

    Add ncurces (6.4) to bootstrap SW

--- IRIX/bootstrap/irix-ports-bootstrap.sh
+++ IRIX/bootstrap/irix-ports-bootstrap.sh
@@ -10,9 +10,14 @@ delete=no
 loc_archive=
 boot_bmake=no
 boot_dialog=no
+boot_ncurses=no
+
 
 bmake_archive="http://www.crufty.net/ftp/pub/sjg/bmake.tar.gz"
 bmake_taz=`basename ${bmake_archive}`
+
+ncurses_archive=https://invisible-island.net/archives/ncurses/ncurses-6.4.tar.gz
+ncurses_taz=`basename ${ncurses_archive}`
 
 dialog_archive=https://invisible-island.net/archives/dialog/dialog.tar.gz
 dialog_taz=`basename ${dialog_archive}`
@@ -28,7 +33,7 @@ TAR=/usr/freeware/bin/tar
 # echo "args: $*"
 
 if [ $# -eq 0 ]; then
-	echo "Usage: '${0} [-c] [-m] [-d] -b /build/dir [-a /archive/local] prefix=/install/dir'."
+	echo "Usage: '${0} [-c] [-m] [-d] [-n] -b /build/dir [-a /archive/local] prefix=/install/dir'."
 else
 
 	if [ ! -d "${SGI_FREEWARE}" ] || [ ! -f `which ${GZCAT}` ]; then
@@ -42,6 +47,7 @@ else
 		case "$1" in
 			-c) delete=yes ;;
 			-m) boot_bmake=yes ;;
+			-n) boot_ncurses=yes ;;
 			-d) boot_dialog=yes ;;
 			-b) build_in=$2 ; shift ;;
 			-a) loc_archive=$2 ; shift ;;
@@ -111,6 +117,40 @@ else
 		fi
 
 		#
+		# get & build ncurses
+		#
+		if [ ${boot_ncurses} = "yes" ]; then
+
+			( cd ${build_in}
+				if [ -f ${ncurses_taz} ]; then
+					rm ${ncurses_taz}
+				fi
+				${WGET} ${ncurses_archive}
+				if [ ! -e ${ncurses_taz} ]; then
+					cp ${loc_archive}/${ncurses_taz} .
+					if [ $? = 0 ]; then
+						echo "Info: found '${ncurses_taz}' in local archive." 
+					else
+						echo "Error: Unable to find or get '${ncurses_taz}'."
+						exit 1 
+					fi
+				fi 
+				${GZCAT} ${ncurses_taz} | ${TAR} -xvf -
+				ncurses_dir=ncurses-6.4
+				( cd ${ncurses_dir}
+					./configure CC=c99 CFLAGS="-64 -mips4 -O2" LDFLAGS="-64 -mips4" CXX=CC CXXFLAGS="-64 -mips4 -LANG:std" --without-debug --prefix=${prefix_dir} 
+					make
+					make install
+				)
+#				if [ $? != 0 ]; then
+#					echo "Error: ncurses build returned failure code, aborting."
+#					exit 1
+#				fi
+
+			)
+		fi
+
+		#
 		# get & build dialog
 		#
 		if [ ${boot_dialog} = "yes" ]; then
@@ -132,11 +172,11 @@ else
 				${GZCAT} ${dialog_taz} | ${TAR} -xvf -
 				dialog_dir=`ls | grep dialog- | cut -f1 -d" "`
 				( cd ${dialog_dir}
-					./configure CC=c99 CFLAGS="-64 -mips4 -O2" LDFLAGS="-64 -mips4" prefix=${prefix_dir} 
+					./configure CC=c99 CFLAGS="-64 -mips4 -O2" LDFLAGS="-64 -mips4" --with-ncurses --prefix=${prefix_dir} 
 					${prefix_dir}/bin/bmake -f makefile 
 					${prefix_dir}/bin/bmake -f makefile install-full
 				)
-#i				if [ $? != 0 ]; then
+#				if [ $? != 0 ]; then
 #					echo "Error: dialog build returned failure code, aborting."
 #					exit 1
 #				fi
@@ -148,6 +188,6 @@ else
 			rm -rf ${build_in}
 		fi
 	else
-		echo "Usage: '${0} [-c] [-m] [-d] -b /build/dir prefix=/install/dir'."
+		echo "Usage: '${0} [-c] [-m] [-d] [-n] -b /build/dir prefix=/install/dir'."
 	fi
 fi
